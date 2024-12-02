59e3aef31ecdb20234e6c111776a3239ca63365d
loisramses <rluisramos666@gmail.com>
2024-12-02 15:02:00 +0000

    improved threading in betano request

--- src/betano/Betano_Sportrequest.py
+++ src/betano/Betano_Sportrequest.py
@@ -10,41 +10,45 @@ async def request_data(fetch_url: str, request_options: dict) -> dict:
     """
     return await page.evaluate(script, await_promise=True)
 
-async def get_event_bets(url: str) -> list:
-    data = await request_data(f'{base_api_url}{url}', {'method': 'GET'})
-    bets = []
-    try: # avoid live events
-        for bet in data['data']['event']['markets']:
-            bet_name = bet['name']
-            options = []
-            for option in bet['selections']:
-                option_name = option['name']
-                option_odd = option['price']
-                options.append((option_name, option_odd))
-            bets.append((bet_name, options))
-    except:
-        pass
-    
-    return bets
+async def get_event_bets(event: dict, semaphore: asyncio.Semaphore) -> dict:
+    async with semaphore:
+        data = await request_data(f'{base_api_url}{event['url']}', {'method': 'GET'})
+        bets = []
+        event_name = event['name'].replace('-', ':')
+        event_url = f'{base_url}{event['url']}'
+        try: # avoid live events
+            for bet in data['data']['event']['markets']:
+                bet_name = bet['name']
+                options = []
+                for option in bet['selections']:
+                    option_name = option['name']
+                    option_odd = option['price']
+                    options.append((option_name, option_odd))
+                bets.append((bet_name, options))
+        except:
+            pass
+        
+        event = defaultdict(lambda: defaultdict(list))
+        event[event_name] = {
+            'url': event_url,
+            'bets': bets
+        }
+        
+        return event
 
 async def get_league_events_and_bets(url: str, league_name: str,  semaphore: asyncio.Semaphore) -> dict | None:
     async with semaphore:
         data = await request_data(f'{base_api_url}{url}', {'method': 'GET'})
         if data['data']['blocks']:
-            events = []
-            for event in data['data']['blocks'][0]['events']:
-                event_name = event['name'].replace('-', ':')
-                event_url = f'{base_url}{event['url']}'
-                events.append({event_name: {
-                        'url': event_url,
-                        'bets': await get_event_bets(event['url'])
-                    }})
-            return { league_name: events}
+            sema = asyncio.Semaphore(3)
+            tasks = [get_event_bets(event, sema) for event in data['data']['blocks'][0]['events']]
+            all_events = await asyncio.gather(*tasks)
+            return { league_name: all_events}
         else:
             return None
 
 async def get_sports():
-    semaphore = asyncio.Semaphore(6)
+    semaphore = asyncio.Semaphore(2)
     data = await request_data(
         'https://www.betano.pt/api/sport/futebol/',
         {'method': 'GET'}
@@ -83,5 +87,3 @@ async def main():
 
 if __name__ == "__main__":
     zd.loop().run_until_complete(main())
-
-# TODO: rever as threads, está muito lento a fazer as chamadas de todos os eventos sequencialmente
